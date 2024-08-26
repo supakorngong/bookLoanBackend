c045b4c3a35fa65ed56e150d1c00dc8be022efb1
supakorngong <supurkorn@hotmail.com>
2024-08-26 12:10:22 +0700

    include info in loanItem

--- src/service/loanItemService.js
+++ src/service/loanItemService.js
@@ -3,6 +3,6 @@ const prisma = require("../model/prisma");
 const bookLoanItemService = {};
 
 bookLoanItemService.findBookLoanItemByLoanId = (loanId) => {
-  return prisma.bookLoanItem.findMany({ where: { bookLoanId: loanId } });
+  return prisma.bookLoanItem.findMany({ where: { bookLoanId: loanId }, include: { book: true } });
 };
 module.exports = bookLoanItemService;
